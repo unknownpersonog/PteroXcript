ba3716b42d0bd4f52f6876818ffb43f71af97ed6
unknownpersonog <104310243+unknownpersonog@users.noreply.github.com>
2022-06-11 23:25:15 -0400

    Initial Changes

--- install.sh
+++ install.sh
@@ -31,17 +31,17 @@ set -e
 SCRIPT_VERSION="v0.11.0"
 GITHUB_BASE_URL="https://raw.githubusercontent.com/vilhelmprytz/pterodactyl-installer"
 
-LOG_PATH="/var/log/pterodactyl-installer.log"
+LOG_PATH="/var/log/pteroscript.log"
 
 # exit with error status code if user is not root
 if [[ $EUID -ne 0 ]]; then
-  echo "* This script must be executed with root privileges (sudo)." 1>&2
+  echo "* Please run this script as root! (sudo)." 1>&2
   exit 1
 fi
 
 # check for curl
 if ! [ -x "$(command -v curl)" ]; then
-  echo "* curl is required in order for this script to work."
+  echo "* curl is not installed and required for script to work."
   echo "* install using apt (Debian and derivatives) or yum/dnf (CentOS)"
   exit 1
 fi
