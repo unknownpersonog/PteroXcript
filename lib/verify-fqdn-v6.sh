76a139618d2fd665608119d451c7ecf973f3c36d
unknownpersonog <104310243+unknownpersonog@users.noreply.github.com>
2022-07-03 09:52:02 -0400

    Update verify-fqdn-v6.sh

--- lib/verify-fqdn-v6.sh
+++ lib/verify-fqdn-v6.sh
@@ -88,7 +88,7 @@ confirm() {
 
 dns_verify() {
   output "Resolving DNS for $fqdn"
-  ip=$(curl -6 -s $CHECKIP_URL)
+  ip=$(curl -6 -sL $CHECKIP_URL)
   dns_record=$(dig +short @$DNS_SERVER "$fqdn" | tail -n1)
   [ "${ip}" != "${dns_record}" ] && fail
   output "DNS verified!"
