f62f050fde243f6a78fbac7c4a16f96dbbba3a94
unknownpersonog <104310243+unknownpersonog@users.noreply.github.com>
2022-07-04 10:01:25 -0400

    Update install-panel.sh

--- install-panel.sh
+++ install-panel.sh
@@ -1038,7 +1038,7 @@ main() {
     # If it's already true, this should be a no-brainer
     [ "$CONFIGURE_LETSENCRYPT" == false ] && ask_assume_ssl
   fi
-  if [[ "$CONFIGURE_LETSENCRYPT" == true ]; then
+  if [ "$CONFIGURE_LETSENCRYPT" == true ]; then
    [ "$ASSUME_SSL" == true ]
   fi
   # verify FQDN if user has selected to assume SSL or configure Let's Encrypt
