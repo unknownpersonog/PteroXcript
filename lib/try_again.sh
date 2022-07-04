a3b2375a352cf60e38fad3e0fec1310cbcef73e8
unknownpersonog <104310243+unknownpersonog@users.noreply.github.com>
2022-07-04 07:55:43 -0400

    Update try_again.sh

--- lib/try_again.sh
+++ lib/try_again.sh
@@ -1,1 +1,15 @@
+dns_verify() {
+  output "Resolving DNS for $fqdn"
+  ip=$(curl -6 -sL $CHECKIP_URL)
+  dns_record=$(dig +short @$DNS_SERVER "$fqdn" AAAA | tail -n1)
+  [ "${ip}" != "${dns_record}" ] && fail
+  output "DNS verified!"
+}
 
+main() {
+  fqdn="$1"
+  dns_verify
+  true
+}
+
+main "$1"
